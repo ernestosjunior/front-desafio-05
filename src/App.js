a7b3a06e9c057f07bb558b0e0a75226232eba4b6
anabeatrizgn <anabeatrizgn.em@gmail.com>
2021-07-27 21:44:05 -0300

    implementação do react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route
+            path="/login"
+            // component={}
+          />
+          <Route
+            path="/usuarios"
+            // component={}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
