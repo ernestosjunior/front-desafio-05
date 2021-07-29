83e7b7e48f35180cf64cae771957649048cfa510
Ernesto Júnior <ernesto.sjunior@hotmail.com>
2021-07-28 21:53:04 -0300

    header

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img />
+      <h1>Pizza Pizzaria & Delivery</h1>
+      <button>Logout</button>
+    </header>
+  );
+};
+
+export default Header;
