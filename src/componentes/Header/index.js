d96c4c81ae67e1619ed7dd855ed25156e6ba212c
Ernesto Júnior <ernesto.sjunior@hotmail.com>
2021-07-31 22:00:37 -0300

    feat: funcao logout

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,16 +1,23 @@
 import "./style.css";
+import { useHistory } from "react-router-dom";
 
 import Logo from "../../assets/pizarria.png";
 
 const Header = () => {
   const path = window.location.pathname;
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.push("/login");
+  };
+
   return (
     <header>
       <img src={Logo} className="header__logo" alt="Logo" />
       <div className="header__conteudo">
         <h1>Pizza Pizzaria & Delivery</h1>
         {!path.includes("/novo-produto") & !path.includes("/editar-produto") ? (
-          <button>Logout</button>
+          <button onClick={() => handleLogout()}>Logout</button>
         ) : (
           ""
         )}
